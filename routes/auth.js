5aee3e06417ab506faaae7f3ff2a1bf5b26c2f12
ANKIT KUMAR <ankitk2703@gmail.com>
2020-12-10 13:29:15 +0530

    Added CreatePost Method

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,6 @@ const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
 
 
-router.get('/', (req, res) => {
-    res.send('Hello')
-})
 
 router.post('/signup', (req, res) => {
     const { name, email, password } = req.body
@@ -31,4 +28,29 @@ router.post('/signup', (req, res) => {
     }
 })
 
+router.post('/signin', (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        return res.status(422).json({ error: "please add email or password" })
+    }
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ error: "Invalid Email or password" })
+            }
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        
+                        res.json({message:"Successfully SignIn"})
+                    }
+                    else {
+                        return res.status(422).json({ error: "Invalid Email or password" })
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+})
 module.exports = router
